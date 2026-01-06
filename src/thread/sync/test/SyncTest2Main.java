f09d7a79b85702d014824fa41077d2291ecc71ff
PeachCoolpis <rlglxh@naver.com>
2026-01-07 00:53:11 +0900

    문제와 풀이

---
+++ src/thread/sync/test/SyncTest2Main.java
@@ -0,0 +1,30 @@
+package thread.sync.test;
+
+import static util.MyLogger.log;
+
+public class SyncTest2Main {
+    public static void main(String[] args) throws InterruptedException {
+        MyCounter myCounter = new MyCounter();
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                myCounter.count();
+            }
+        };
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-2");
+        thread1.start();
+        thread2.start();
+    }
+    static class MyCounter {
+        public void count() {
+            //지연변수는 임계영역에서 안전
+            //쓰레드가 서로 공유하고 있지 않기때문이다.
+            int localValue = 0;
+            for (int i = 0; i < 1000; i++) {
+                localValue = localValue + 1;
+            }
+            log("결과: " + localValue);
+        }
+    }
+}
